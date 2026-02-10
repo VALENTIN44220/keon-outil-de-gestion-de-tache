d66b9cdfb1043b00ebedd7303bcac3eb65ec7946
VALENTIN44220 <valentin.bertrand@keon-group.com>
2026-02-10 16:12:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 // src/main.tsx
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +15,7 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
