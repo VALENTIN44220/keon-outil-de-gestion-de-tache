062fed64a646f9d67d08bd9ef06f0428e864caaa
VALENTIN44220 <valentin.bertrand@keon-group.com>
2026-02-10 15:32:09 +0100

    Refactor main.tsx to include React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 // src/main.tsx
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,8 +14,10 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
